96ca6a7ec894490881946c2965e5b1a5905ad3a2
dpdani <dani.parmeggiani@gmail.com>
2016-06-02 16:35:58 +0200

    Enhanced serial_recognizer to filter out unwanted characters.

---
+++ src/peripherals/TEST001.py
@@ -0,0 +1,12 @@
+import peripherals.peripheral as peripheral
+import peripherals.commands as commands
+
+
+class TEST001(peripheral.Peripheral):
+    supported_firmwares = [100]
+    commands = [
+        commands.UnrecognizedCommand(),
+        commands.Info(),
+        commands.ToggleLed(),
+        commands.ReadButton(),
+    ]
