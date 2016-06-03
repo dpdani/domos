434e2140c58c5da9a49585ef21e0480e4b5209ff
ChiavacciPI <chiavacci@PI.pi>
2016-06-03 16:31:43 +0200

    ci sta

--- src/ports_listener.py
+++ src/ports_listener.py
@@ -18,20 +18,23 @@ DEFAULT_BAUDRATE = 9600
 # ]
 DEFAULT_PORTS = [x[0] for x in serial.tools.list_ports.comports()]
 
+def get_ports():
+  return [x[0] for x in serial.tools.list_ports.comports()]
+
 
 def loop_start_listening():
     threads, serials, stop_event = start_listening()
 
     def listen_loop(threads, serials, stop_event):
-        old_available_ports = serial.tools.list_ports.comports()
+        old_available_ports = get_pèorts()
         while not stop_event.is_set():
-            if old_available_ports != serial.tools.list_ports.comports():
+            if old_available_ports != get_ports():
                 new_threads, new_serials, _ = start_listening(stop_event)
                 for new_thread in new_threads:
                     threads.append(new_thread)
                 for new_serial in new_serials:
                     serials.append(new_serial)
-                old_available_ports = serial.tools.list_ports.comports()
+                old_available_ports = get_ports()
 
     thread = threading.Thread(target=listen_loop, args=(threads, serials, stop_event),
                                   name='Main listening thread')
@@ -46,7 +49,7 @@ def start_listening(stop_event=None):
     threads = []
     ports = []
     serials = []
-    for port in serial.tools.list_ports.comports():
+    for port in get_ports():
         thread = threading.Thread(target=listen_port, args=(port, stop_event, serials),
                                   name='Thread listening on port {}'.format(port))
         thread.start()
